35e7402500ff1c436c7a5bfc29a474956e2c4a48
vipdou <84176711+vipdou@users.noreply.github.com>
2023-07-05 19:58:29 +0800

    Create index.ts

---
+++ config/index.ts
@@ -0,0 +1,37 @@
+﻿import { AiFillGithub } from 'react-icons/ai';
+import avatarJpg from '@/public/avatar.jpg';
+
+export const config: IConfig = {
+  name: 'haotian',
+  avatar: avatarJpg,
+  contact: [
+    {
+      icon: AiFillGithub,
+      url: 'https://www.github.com/vipdou'
+    }
+  ],
+  navList: [
+    {
+      title: 'Archives',
+      url: '/archives'
+    },
+    {
+      title: 'About',
+      url: '/about'
+    }
+  ],
+  emoji: [
+    '😜',
+    '🤤',
+    '🤪',
+    '😳',
+    '😍',
+    '❤️',
+    '🤭',
+    '🥰',
+    '😵‍💫',
+    '😇',
+    '😓',
+    '🤣'
+  ]
+};
